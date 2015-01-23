59d8146ae50bc53ec5cd528231248fd06bbcb530
shimoji.koichi <shimoji.koichi@suzukishouten.co.jp>
2015-01-23 14:38:24 +0900

    add simple api

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -36,6 +36,14 @@
  */
 	CakePlugin::routes();
 
+/*
+ * API
+ */
+Router::mapResources(array (
+	'todo_lists',
+));
+Router::parseExtensions('json');
+
 /**
  * Load the CakePHP default routes. Only remove this if you do not want to use
  * the built-in default routes.
